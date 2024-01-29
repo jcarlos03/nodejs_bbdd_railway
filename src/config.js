bf378201f63a300004d1577de388b45c293722d0
Jose Carlos <jcarlosfpasir@gmail.com>
2024-01-29 17:09:28 +0100

    Comentario

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 3000
+export const DB_HOST = 'localhost'
+export const DB_NAME = 'andel'
+export const DB_USER = 'root'
+export const DB_PASSWORD = '' 
+export const DB_PORT = 3306
+        

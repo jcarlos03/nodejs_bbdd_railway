74347f75f32b7594225e623b7b90e87b6c89b619
Jose Carlos <jcarlosfpasir@gmail.com>
2024-01-29 17:31:47 +0100

    Config modificado

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000
-export const DB_HOST = 'localhost'
-export const DB_NAME = 'andel'
+export const DB_HOST = 'monorail.proxy.rlwy.net'
+export const DB_NAME = 'railway'
 export const DB_USER = 'root'
-export const DB_PASSWORD = '' 
-export const DB_PORT = 3306
+export const DB_PASSWORD = 'GDa36d-gdg6Da21a43BHG1FgCEc6eDeC' 
+export const DB_PORT = 23257
         
